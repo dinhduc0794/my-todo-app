bebadba53fb67febc793a06510cb4b70b425c4a4
dinhduc0794 <ngdduc79@gmail.com>
2025-04-21 16:07:57 +0700

    docs: remove tab

--- TodoApp/Models/Task.cs
+++ TodoApp/Models/Task.cs
@@ -1,19 +1,19 @@
-﻿    using TodoApp.Enums;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
+﻿using TodoApp.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-    namespace TodoApp.Models;
+namespace TodoApp.Models;
 
-    public class Task : BaseModel
-    {
-        [Key]
-        public int TaskId { get; set; } 
-        [Required]
-        public string Title { get; set; }
-        public string? Description { get; set; }
-        public bool IsCompleted { get; set; } = false;
-        public DateTime? DueDate { get; set; }
-        public int? CategoryId { get; set; }
-        [ForeignKey("CategoryId")] public Category? Category { get; set; }
-        public Priority? Priority { get; set; }
-    }
+public class Task : BaseModel
+{
+    [Key]
+    public int TaskId { get; set; } 
+    [Required]
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; } = false;
+    public DateTime? DueDate { get; set; }
+    public int? CategoryId { get; set; }
+    [ForeignKey("CategoryId")] public Category? Category { get; set; }
+    public Priority? Priority { get; set; }
+}
